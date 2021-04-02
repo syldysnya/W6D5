c91782bc6eb8b84d493c1f95b6e8e04f545be15b
syldysnya <syldysnya@gmail.com>
2021-04-02 16:13:41 -0400

    Show method

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -5,9 +5,16 @@ class CatsController < ApplicationController
         render :index
     end
 
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
+    end
 
+    private
 
-
+    def cat_params
+        params.require(:cats).permit(:name, :sex, :color, :birth_date, :description)
+    end
 
 
 end
