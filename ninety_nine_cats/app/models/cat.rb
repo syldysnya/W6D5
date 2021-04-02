51f18205854a7a87e1050833d0264ec3ef288a53
syldysnya <syldysnya@gmail.com>
2021-04-02 15:10:07 -0400

    Finish models (hope so)

--- ninety_nine_cats/app/models/cat.rb
+++ ninety_nine_cats/app/models/cat.rb
@@ -1,8 +1,24 @@
+require "action_view"
+
 class Cat < ApplicationRecord
+    
+    include ActionView::Helpers::DateHelper
 
-    validates: :color, inclusion: {in: ['black', 'white', 'brown', 'orange']}
-    validates: :name, :sex, :color, :birth_date, :description
+    COLORS = [
+        'black',
+        'white',
+        'brown',
+        'orange'
+    ]
 
+    validates :color, inclusion: { in: COLORS }
+    validates :name, :sex, :color, :birth_date, :description, presence: true
+    validates :sex, inclusion: { in: ['M', 'F'] }
 
+    def age
+        time_ago_in_words(birth_date)
+    end
 
 end
+
+# k = Cat.new(name: 'Kat', sex: 'M', color: 'orange', birth_date: 2015/01/20, description: 'namsjhdkehe')
